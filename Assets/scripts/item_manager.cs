58ad88a68b9cd23e799bd85110ea4670175a6936
krenar shabani <krenarshabani@gmail.com>
2016-04-29 13:31:17 -0400

    second commit

---
+++ Assets/scripts/item_manager.cs
@@ -0,0 +1,91 @@
+﻿/*
+    -Make item movement snappier (done)Note : the horizontal edges are shitty
+
+*/
+
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class item_manager: MonoBehaviour {
+    private Component[] items;
+    private static float xinitial = 9.2f;
+    private static float yinitial = 4f;
+    protected static float xshift = 2.1f;
+    protected static float yshift = 2.4f;
+    
+    private static float x = 9.2f;
+    private static float y = 4f;
+    private static int cols = 7;
+    private static int numerator;
+
+    void Start ()
+    {
+        items = GetComponentsInChildren<item>();
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            items[i].tag = UnityEditorInternal.InternalEditorUtility.tags[items[i].GetComponent<item>().tagSet];
+            if (items[i].GetComponent<Renderer>().enabled == false)
+                items[i].GetComponent<Renderer>().enabled = true;
+            setPos(i);
+
+            items[i].GetComponentInChildren<text>().setNewNum();
+            numerator = i;
+        }
+    }
+    public void setPos(GameObject item)
+    {
+        x = xinitial;
+        y = yinitial;
+        numerator = 0;
+        while (true)
+        {
+            if (numerator >= cols)
+            {
+                y -= yshift;
+                cols += cols;
+                x += xshift * cols;
+            }
+            x -= xshift;
+            numerator++;
+            
+            item.transform.localPosition = new Vector2(x, y);
+            int hitColliders = (Physics.OverlapBox(item.transform.position, new Vector3(1, 1, 1))).Length;
+            if (hitColliders < 3)
+            {
+                item.transform.localPosition = new Vector2(x, y);
+                break;
+            }
+        }
+    }
+    void setPos(int i)
+    {
+        if (i >= cols)
+        {
+            y -= yshift;
+            cols += cols;
+            x += xshift * cols;     
+        }
+        x -= xshift;
+        items[i].transform.localPosition = new Vector2(x, y);
+    }
+
+    public void fuckunityfam(int[] needed, string[] recipe)
+    {
+        StartCoroutine(cleanUp(needed, recipe));
+    }
+    private IEnumerator cleanUp(int[] needed, string[] recipe)
+    {
+        for (int i = 0; i < needed.Length; i++)
+        {
+            GameObject[] temp = GameObject.FindGameObjectsWithTag(recipe[i]);
+            for (int j = 0; j < needed[i]; j++)
+            {
+                Destroy(temp[j]);
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/scripts/recipie.cs
@@ -0,0 +1,118 @@
+﻿/*
+- Make better "ingredients needed: " display
+
+*/
+
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class recipie : MonoBehaviour {
+    private GameObject parent;
+    private GameObject createe;
+    public TextMesh requirements;
+    private string[] recipe;
+    private int[] needed;
+    private int[] ingredients;
+    private bool flag = true;
+    private Vector3 loc = new Vector3(5.3f, 6, 0);
+
+    void start()
+    {
+        requirements.characterSize = 10;
+       // requirements.fontSize = 30;
+        //requirements.text = ("Select a recipe!");
+    }
+    public void updateRecipe(String[] re, int[] ne, GameObject cre)
+    {
+        
+        GameObject[] old = GameObject.FindGameObjectsWithTag("display");
+        for (int i = 0; i < old.Length; i++)
+        {
+            Destroy(old[i]);
+        }
+        
+        recipe = re;
+        needed = ne;
+        createe = cre;
+
+
+        string display = "\t";
+        //display = ("you need:\t");
+        GameObject temp;
+        parent = GameObject.FindGameObjectWithTag("itemmanager");
+        float x = 0;
+        for (int i = 0; i < recipe.Length; i++)
+        {
+            temp = (GameObject)Instantiate(GameObject.FindGameObjectWithTag(recipe[i].Substring(0,recipe[i].Length-5)),loc, Quaternion.identity);
+            temp.transform.parent = parent.transform;
+            temp.transform.localPosition = new Vector3(loc.x+x, loc.y);
+            temp.GetComponentInChildren<text>().setNull();
+            temp.GetComponent<Rigidbody>().detectCollisions = false;
+            temp.tag = ("display");
+            x -= 3.5f;
+            display += (" x" + needed[i] + " +\t ");
+        }
+        display = display.Substring(0, display.Length - 3);
+        display += '=';
+        temp = (GameObject)Instantiate(createe, loc, Quaternion.identity);
+        temp.transform.parent = parent.transform;
+        temp.transform.localPosition = new Vector3(loc.x + x, loc.y);
+        temp.GetComponentInChildren<text>().setNull();
+        temp.GetComponent<Rigidbody>().detectCollisions = false;
+        temp.tag = ("display");
+        x -= 3.5f;
+        print(display);
+        requirements.text = display;
+    }
+    public void timeToCraft()
+    {
+        if (recipe == null & needed == null)
+        {
+            print("no recipies selected! (or you didnt calibrate the recipe correctly?)");
+            return;
+        }
+        ingredients = new int[recipe.Length];
+        for (int i = 0; i < recipe.Length; i++)
+        {
+            ingredients[i] = GameObject.FindGameObjectsWithTag(recipe[i]).Length;
+            if (ingredients[i] >= needed[i])
+            {
+                flag = true;
+            }
+            else
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+        {
+            print("we can craft!");
+        }
+        else
+        {
+            print("we cannot craft!");
+        }
+        parent = GameObject.FindGameObjectWithTag("itemmanager");
+        if (flag)
+        {
+            if (GameObject.FindGameObjectWithTag(UnityEditorInternal.InternalEditorUtility.tags[createe.GetComponent<item>().tagSet]))
+            {
+                GameObject newmat = GameObject.FindGameObjectWithTag(UnityEditorInternal.InternalEditorUtility.tags[createe.GetComponent<item>().tagSet]);
+                newmat.GetComponent<item>().increment();
+                newmat.GetComponentInChildren<text>().setNewNum();
+            }
+            else
+            {
+                GameObject New = Instantiate(createe);
+                New.transform.parent = GameObject.Find("items").transform;
+                parent.GetComponent<item_manager>().setPos(New);
+                New.GetComponent<item>().setAmount(1);
+                New.GetComponentInChildren<text>().setNewNum();
+                New.tag = UnityEditorInternal.InternalEditorUtility.tags[New.GetComponent<item>().tagSet];
+            }
+            parent.GetComponent<item_manager>().fuckunityfam(needed,recipe);
+        }
+    }
+}
